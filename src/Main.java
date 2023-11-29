edcfcb2dfe47171a2460fcf45925d1fcd2fcf855
dqpqt8 <dqpqt8@umsl.edu>
2023-11-29 01:29:11 -0600

    Final Commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
 import java.lang.reflect.Array;
 import java.util.*;
 
+/*
+Project 2
+David Pham
+Francis Rodolph
+ */
+
 public class Main {
     public static void main(String[] args) {
         Simulation obj1 = new Simulation();
